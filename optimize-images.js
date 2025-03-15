2a2c0153bb20fe78c16bfb9fe77d14ef91f9a8d6
Manish Sherawat <sherawat003@gmail.com>
2025-03-15 17:38:26 +0530

    Update optimize-images.js

--- optimize-images.js
+++ optimize-images.js
@@ -9,17 +9,16 @@ if (!fs.existsSync(outputDir)){
     fs.mkdirSync(outputDir);
 }
 
-fs.readdir(imagesDir, (err, files) => {
+fs.readdir(imagesDir, async (err, files) => {
     if (err) throw err;
 
-    files.forEach(file => {
+    await Promise.all(files.map(file => {
         const ext = path.extname(file).toLowerCase();
         if (ext === '.jpg' || ext === '.jpeg' || ext === '.png') {
-            sharp(path.join(imagesDir, file))
-                .toFile(path.join(outputDir, `${path.basename(file, ext)}.webp`), (err, info) => {
-                    if (err) console.error(`Error processing ${file}:`, err);
-                    else console.log(`Converted ${file} to WebP format.`);
-                });
+            return sharp(path.join(imagesDir, file))
+                .toFile(path.join(outputDir, `${path.basename(file, ext)}.webp`), { quality: 80 })
+                .then(info => console.log(`Converted ${file} to WebP format. Size: ${info.size} bytes`))
+                .catch(err => console.error(`Error processing ${file}:`, err));
         }
-    });
+    }));
 });
